d303ad49d01954347d23b7c88224063312932a23
arceister <imandajagad01@gmail.com>
2022-04-16 04:52:58 +0700

    Test: Add testcase for `POST #create` method

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -27,4 +27,16 @@ describe FoodsController do
     it "assigns the requested food to @food"
     it "renders the :edit template"
   end
+
+  describe 'POST #create' do
+    context "with valid attributes" do
+      it "saves the new food in the database"
+      it "redirects to foods#show"
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new food in the database"
+      it "re-renders the :new template"
+    end
+  end
 end
