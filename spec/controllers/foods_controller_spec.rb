c8d568be0e21ded88c3291575d465191447f01f7
arceister <imandajagad01@gmail.com>
2022-04-16 04:52:18 +0700

    Test: Add testcase for `GET #new` method

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -17,4 +17,9 @@ describe FoodsController do
     it "assigns the requested food to @food"
     it "renders the :show template"
   end
+
+  describe 'GET #new' do
+    it "assigns a new Food to @food"
+    it "renders the :new template"
+  end
 end
