85e7b2b387cd8f8a997b0cf661fee93965bd2453
arceister <imandajagad01@gmail.com>
2022-04-16 04:54:29 +0700

    Test: Add testcase for `DELETE #destroy` method

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -52,4 +52,9 @@ describe FoodsController do
       it "re-renders the :edit template"
     end
   end
+
+  describe 'DELETE #destroy' do
+    it "deletes the food from the database"
+    it "redirects to foods#index"
+  end
 end
